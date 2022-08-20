63b5f491b314200a406487cba079ede07eac03fc
Igor Prusov <prusovigor@gmail.com>
2022-08-20 20:39:43 +0300

    Continue FSM refactoring

--- src/prefix_fsm.rs
+++ src/prefix_fsm.rs
@@ -3,57 +3,67 @@ pub enum Event {
     Number(u64),
 }
 
-pub enum TState {
+pub enum TState<T> {
     Empty,
-    Ambiguous(Vec<String>),
-    Selected(String),
+    Ambiguous(String, Vec<T>),
+    Selected(T),
 }
 
-pub trait Single {}
-pub trait Multiple {
-    fn len(&self) -> usize;
-    fn at(&self, index: usize) -> dyn Single;
+pub trait Item {
+    fn update(&self, x: u64);
 }
 
-pub struct Fsm<F, G>
+pub struct Fsm<T, F>
 where
-    F: Fn(&str) -> TState,
-    G: Fn(&str) -> String,
+    F: Fn(&str) -> Vec<T>,
 {
-    pub state: TState,
+    pub state: TState<T>,
     pub from_prefix: F,
-    pub print_one: G,
 }
 
-impl<F: Fn(&str) -> TState, G: Fn(&str) -> String> Fsm<F, G> {
+impl<T: Clone + Item, F: Fn(&str) -> Vec<T>> Fsm<T, F> {
+    fn vec_to_state(prefix: &str, v: Vec<T>) -> TState<T> {
+        match v.len() {
+            0 => TState::Empty,
+            1 => TState::Selected(v[0].clone()),
+            _ => TState::Ambiguous(String::from(prefix), v),
+        }
+    }
+
     pub fn next(&mut self, event: Event) {
         self.state = match (&self.state, event) {
             /* From Empty */
             (TState::Empty, Event::Number(_)) => TState::Empty,
-            (TState::Empty, Event::Text(s)) => (self.from_prefix)(&s),
+            (TState::Empty, Event::Text(s)) => {
+                let v = (self.from_prefix)(&s);
+                Self::vec_to_state(s.as_str(), v)
+            }
 
             /* From Ambiguous */
-            (TState::Ambiguous(vec), event) => match event {
+            (TState::Ambiguous(prefix, vec), event) => match event {
                 Event::Number(x) if (x as usize) < vec.len() => {
                     TState::Selected(vec[x as usize].clone())
                 }
-                _ => TState::Ambiguous(vec.clone()),
+                _ => TState::Ambiguous(prefix.clone(), vec.clone()),
             },
 
             /* From Selected */
-            (TState::Selected(reg), Event::Number(x)) => TState::Selected(reg.clone()),
+            (TState::Selected(reg), Event::Number(x)) => {
+                reg.update(x);
+                TState::Selected(reg.clone())
+            }
 
-            (TState::Selected(_), Event::Text(prefix)) => (self.from_prefix)(&prefix),
+            (TState::Selected(_), Event::Text(s)) => {
+                let v = (self.from_prefix)(&s);
+                Self::vec_to_state(s.as_str(), v)
+            }
         }
     }
 
-    /*
-    pub fn prompt(&self) -> String {
-        match &self.state {
-            TState::Empty => String::from(""),
-            TState::Ambiguous(vec) => vec[0].clone(),
-            TState::Selected(name) => (self.print_one)(name),
+    pub fn new(f: F) -> Self {
+        Fsm {
+            state: TState::Empty,
+            from_prefix: f,
         }
     }
-    */
 }
--- src/tui_fsm.rs
+++ src/tui_fsm.rs
@@ -1,209 +1,48 @@
 use std::{
     collections::BTreeMap,
+    fmt,
     io::{self, Write},
 };
 
-use crate::prefix_fsm::{Event, Fsm, Multiple, Single, TState};
+use crate::prefix_fsm::{Event, Fsm, Item, TState};
 use mra_parser::RegisterDesc;
 
-/*
-
-struct RegisterInfo<'a> {
-    reg: &'a RegisterDesc,
-    value: Option<u64>,
-}
-
 #[derive(Clone)]
-struct RegisterSubset<'a> {
-    vec: Vec<&'a RegisterDesc>,
-    prefix: String,
+struct Elem<'a>(&'a RegisterDesc);
+
+impl<'a> Item for Elem<'a> {
+    fn update(&self, x: u64) {
+        println!("value = {}", x)
+    }
 }
 
-
-
-
-struct Fsm<'a, T: Single<'a>, U: Multiple<'a>> {
-    data: FsmData<'a>,
-    state: TState<T, U>,
-}
-
-impl<'a> fmt::Display for RegisterInfo<'a> {
+impl<'a> fmt::Display for Elem<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "{}", self.reg)?;
-        if let Some(x) = self.value {
-            writeln!(f, "value = {}", x)?;
-        };
-        Ok(())
+        write!(f, "{}", self.0)
     }
 }
 
-impl<'a> Single<'a> for RegisterInfo<'a> {
-    fn new(reg: &'a RegisterDesc, value: Option<u64>) -> Self {
-        RegisterInfo {
-            reg: reg,
-            value: value,
-        }
-    }
-
-    fn get_reg(&self) -> &'a RegisterDesc {
-        self.reg
+fn get_prompt<'a>(state: &'a TState<Elem>) -> &'a str {
+    match &state {
+        TState::Empty => "",
+        TState::Ambiguous(prefix, _) => prefix,
+        TState::Selected(reg) => reg.0.name.as_ref(),
     }
 }
-
-impl<'a> Multiple<'a> for RegisterSubset<'a> {
-    fn new(vec: Vec<&'a RegisterDesc>, prefix: &str) -> Self {
-        RegisterSubset {
-            vec,
-            prefix: String::from(prefix),
-        }
-    }
-
-    fn len(&self) -> usize {
-        self.vec.len()
-    }
-
-    fn at(&self, index: usize) -> &'a RegisterDesc {
-        &self.vec[index]
-    }
-
-    fn get_prefix(&self) -> &str {
-        self.prefix.as_ref()
-    }
-}
-
-impl<'a> fmt::Display for RegisterSubset<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for (i, reg) in self.vec.iter().enumerate() {
-            writeln!(f, "{}: {}", i, reg.name)?;
-        }
-        Ok(())
-    }
-}
-
-impl<'a, 'b, T: Single<'b>, U: Multiple<'b>> TState<T, U> {
-    fn from_prefix(prefix: &str, data: &'a FsmData<'b>) -> TState<T, U> {
-        let m = data.select(prefix);
-        match m.len() {
-            0 => TState::Empty,
-            1 => TState::Selected(T::new(m[0], None)),
-            _ => TState::Ambiguous(U::new(m, prefix)),
-        }
-    }
-}
-
-impl<'a, T: Single<'a>, U: Multiple<'a> + Clone> Fsm<'a, T, U> {
-    fn new(data: &'a BTreeMap<String, RegisterDesc>) -> Fsm<'a, T, U> {
-        let d = FsmData::new(data);
-        Fsm {
-            data: d,
-            state: TState::Empty,
-        }
-    }
-    fn next(&mut self, event: Event) {
-        self.state = match (&self.state, event) {
-            /* From Empty */
-            (TState::Empty, Event::Number(_)) => TState::Empty,
-            (TState::Empty, Event::Text(s)) => TState::from_prefix(&s, &self.data),
-
-            /* From Ambiguous */
-            (TState::Ambiguous(subset), event) => match event {
-                Event::Number(x) if (x as usize) < subset.len() => {
-                    TState::Selected(T::new(subset.at(x as usize), None))
-                }
-                _ => TState::Ambiguous(subset.clone()),
-            },
-
-            /* From Selected */
-            (TState::Selected(reg), Event::Number(x)) => {
-                TState::Selected(T::new(&reg.get_reg(), Some(x)))
-            }
-
-            (TState::Selected(_), Event::Text(value)) => TState::from_prefix(&value, &self.data),
-        };
-    }
-
-    fn current(&'a self) -> &'a TState<T, U> {
-        &self.state
-    }
-
-    fn prompt(&self) -> &str {
-        match &self.state {
-            TState::Empty => "",
-            TState::Ambiguous(subset) => subset.get_prefix().as_ref(),
-            TState::Selected(reg) => reg.get_reg().name.as_ref(),
-        }
-    }
-}
-
-struct FsmData<'a> {
-    data: &'a BTreeMap<String, RegisterDesc>,
-}
-
-impl<'a> FsmData<'a> {
-    fn new(data: &'a BTreeMap<String, RegisterDesc>) -> FsmData<'a> {
-        FsmData { data }
-    }
-    fn select(&self, prefix: &str) -> Vec<&'a RegisterDesc> {
-        let it = self
-            .data
-            .range(String::from(prefix)..)
-            .take_while(|x| x.0.starts_with(&prefix));
-        it.map(|(_, v)| v).collect()
-    }
-}
-
-*/
-/*
-struct SingleString(String);
-struct MultipleStrings(Vec<String>);
-
-impl Single for SingleString {
-}
-
-impl Multiple for MultipleStrings {
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-
-    fn at(&self, index: usize) -> dyn Single {
-        SingleString(self.0[index])
-    }
-}
-
- */
 
 pub fn run_tui(data: &BTreeMap<String, RegisterDesc>) -> io::Result<()> {
-    let f = |prefix: &str| -> TState {
+    let mut fsm = Fsm::new(|prefix: &str| -> Vec<Elem> {
         let it = data
             .range(String::from(prefix)..)
             .take_while(|x| x.0.starts_with(&prefix));
 
-        let v: Vec<String> = it.map(|(_, v)| v.name.clone()).collect();
+        it.map(|(_, v)| Elem(v)).collect()
+    });
 
-        match v.len() {
-            0 => TState::Empty,
-            1 => TState::Selected(v[0].clone()),
-            _ => TState::Ambiguous(v),
-        }
-    };
-    let print_one = |name: &str| -> String {
-        match data.get(name) {
-            Some(x) => format!("{}", x),
-            None => {
-                let msg = format!("Can't find {}:", name);
-                panic!("{}", msg);
-            }
-        }
-    };
-    let mut fsm = Fsm {
-        state: TState::Empty,
-        from_prefix: f,
-        print_one,
-    };
     println!("Enter register names:");
-    print!("> ");
-    io::stdout().flush()?;
     loop {
+        print!("{}> ", get_prompt(&fsm.state));
+        io::stdout().flush()?;
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
 
@@ -218,25 +57,12 @@ pub fn run_tui(data: &BTreeMap<String, RegisterDesc>) -> io::Result<()> {
         };
 
         fsm.next(event);
-        match &fsm.state {
-            TState::Selected(name) => {
-                let name = name.to_lowercase();
-                let reg = data.get(&name).expect("Should always find this element");
-                println!("{}", reg);
-                print!("{} > ", reg.name);
-                io::stdout().flush()?;
-            }
-
-            TState::Ambiguous(v) => {
-                for (i, x) in v.iter().enumerate() {
-                    println!("{}) {}", i, x)
-                }
-                print!("> ");
-                io::stdout().flush()?;
-            }
-            TState::Empty => {
-                print!("> ");
-                io::stdout().flush()?;
+        if let TState::Selected(el) = &fsm.state {
+            println!("{}", el.0);
+        }
+        if let TState::Ambiguous(_, v) = &fsm.state {
+            for (i, x) in v.iter().enumerate() {
+                println!("{}) {}", i, x.0.name)
             }
         }
     }
